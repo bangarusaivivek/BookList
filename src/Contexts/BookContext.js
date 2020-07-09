7de2175a167f4bd1ebce8d438b4651398b400815
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-07-09 17:22:44 +0530

    feat: add and remove book functionalites

--- src/Contexts/BookContext.js
+++ src/Contexts/BookContext.js
@@ -1,4 +1,5 @@
 import React,{ createContext, useState } from 'react'
+import uuid from 'uuid/dist/v1';
 
 export const BookContext = createContext();
 
@@ -7,10 +8,17 @@ function BookContextProvider(props) {
         {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
         {title: 'the final empire', author: 'brandon sanderson', id: 2},
     ])
+    const addBook = (title, author) => {
+        setBooks([...books,{title: title,author: author,id: uuid()}])
+    }
+    const removeBook = (id) => {
+        const remainedBooks = books.filter((book)=> book.id !== id)
+        setBooks(remainedBooks);
+    }
     return (
-        <div>
-            
-        </div>
+        <BookContext.Provider value={{books, addBook, removeBook}}>
+            {props.children}
+        </BookContext.Provider>
     )
 }
 
