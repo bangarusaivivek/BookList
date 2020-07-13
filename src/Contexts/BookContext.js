765a43679eca3394e18985bfe8c40977782a2d45
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-07-14 01:37:44 +0530

    added localstorage

--- src/Contexts/BookContext.js
+++ src/Contexts/BookContext.js
@@ -1,4 +1,4 @@
-import React,{ createContext, useState, useReducer } from 'react'
+import React,{ createContext, useState, useReducer, useEffect } from 'react'
 import uuid from 'uuid/dist/v1';
 import { bookReducer } from '../reducers/bookReducer';
 
@@ -8,7 +8,10 @@ function BookContextProvider(props) {
     // const [books,dispatch] = useReducer(bookReducer,[]);
 
 
-    const [books, setBooks] = useState([])
+    const [books, setBooks] = useState(()=>{
+        const localData = localStorage.getItem('books');
+        return localData ? JSON.parse(localData) : []
+    })
     const addBook = (title, author) => {
         setBooks([...books,{title: title,author: author,id: uuid()}])
     }
@@ -16,6 +19,10 @@ function BookContextProvider(props) {
         const remainedBooks = books.filter((book)=> book.id !== id)
         setBooks(remainedBooks);
     }
+    useEffect(() => {
+        localStorage.setItem('books',JSON.stringify(books))
+    },[books]);
+
     return (
         <BookContext.Provider value={{books, addBook, removeBook}}>
             {props.children}
